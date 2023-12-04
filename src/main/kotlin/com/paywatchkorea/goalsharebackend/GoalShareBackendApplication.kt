0e0dc1d7e593168891fbeabea72e45e9c6c7a44e
myeongjin yang <78643271+audwls624@users.noreply.github.com>
2023-12-04 16:15:18 +0900

    Goal Share Backend - Initial Commit

---
+++ src/main/kotlin/com/paywatchkorea/goalsharebackend/GoalShareBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.paywatchkorea.goalsharebackend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GoalShareBackendApplication
+
+fun main(args: Array<String>) {
+	runApplication<GoalShareBackendApplication>(*args)
+}
---
+++ src/test/kotlin/com/paywatchkorea/goalsharebackend/GoalShareBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.paywatchkorea.goalsharebackend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GoalShareBackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
